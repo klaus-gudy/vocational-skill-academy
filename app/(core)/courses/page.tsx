45447128d5aac4d7d68e0d31bad62f8e93ffa3c1
Klaus-gudy <klausongudy@gmail.com>
2025-02-15 11:42:28 +0300

    Feature: Added CoursesPage with related components to render CourseExplorer

---
+++ app/(core)/courses/page.tsx
@@ -0,0 +1,11 @@
+import CourseExplorer from "@/components/courses/course-explorer";
+
+export default function CoursesPage() {
+    return (
+      <main className="min-h-screen">
+        <div className="">
+          <CourseExplorer />
+        </div>
+      </main>
+    )
+  }
